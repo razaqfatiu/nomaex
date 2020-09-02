c2a22b22f7fbe8957386f17ec4618eb10c3a0e7e
Fatiu Isiaka <fatiuisiaka@MacBook-Pro.local>
2020-09-02 12:11:32 +0100

    add cors policy

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 // app.use(cors());
-app.use(cors({ credentials: true, origin: process.env.frontEndURL }));
+// app.use(cors({ credentials: true, origin: process.env.frontEndURL }));
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', process.env.frontEndURL);
