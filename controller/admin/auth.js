8430bb83680e0afa1b733b5a58a4f8d2cddca742
Fatiu Isiaka <fatiuisiaka@MacBook-Pro.local>
2020-09-05 20:35:44 +0100

    set cookie to same site none

--- controller/admin/auth.js
+++ controller/admin/auth.js
@@ -245,8 +245,7 @@ module.exports = {
         res.cookie('token', signToken, {
           maxAge: oneDayCookie, // 24 hour
           httpOnly: true,
-          // sameSite: false,
-          // secure: true,
+          sameSite: none,
           secure: process.env.NODE_ENV === 'production' ? true : false,
           domain: process.env.domain
         })
