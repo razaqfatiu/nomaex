cc1d794d498c677827fc4d3ee1bd8370298f1b55
razaqfatiu <razaqfatiu@gmail.com>
2021-05-06 10:50:45 +0100

    fix: email link redirect to frontend

--- helper/mail.js
+++ helper/mail.js
@@ -1,22 +1,19 @@
-const { transporter } = require("../config/nodemailer");
-require("dotenv").config();
-const parseEmailContent = require("../config/nodemailer");
+const { transporter } = require('../config/nodemailer');
+require('dotenv').config();
+const parseEmailContent = require('../config/nodemailer');
 
 module.exports = {
-  
   forgotPassword(req, userInfo) {
     (async () => {
       try {
         emailConfig = {
           from: process.env.NOMAEX_EMAIL,
           to: userInfo.email,
-          subject: "Password Reset",
+          subject: 'Password Reset',
           text: `Dear ${userInfo.name}, \n\n
                 You are receiving this because you (or someone else) have requested the reset of the password for your account.\n
                 Please click on the following link, or paste this into your browser to complete the process:\n
-                ${req.protocol}://${req.get("host").slice(0, -4)}${
-            process.env.frontEndPort
-          }/reset-password/${userInfo.token}\n
+                ${process.env.frontEndURLProd}/reset-password/${userInfo.token}\n
                 If you did not request this, please ignore this email and your password will remain unchanged.\n\n
                 Best regards,
                 Nomaex.
@@ -36,13 +33,11 @@ module.exports = {
         emailConfig = {
           from: process.env.NOMAEX_EMAIL,
           to: userInfo.email,
-          subject: "Account Activation",
+          subject: 'Account Activation',
           text: `Dear ${userInfo.name}, \n\n
           Thank you for signing up with NOMAEX.\n
           To verify your account, Please click on the following link, or paste the link into your browser to complete the process:\n
-          ${req.protocol}://${req.get("host").slice(0, -4)}${
-            process.env.frontEndPort
-          }/verify/user/${userInfo.token}\n
+          ${process.env.frontEndURLProd}/verify/user/${userInfo.token}\n
           If you did not request this, please ignore this email.\n\n
           Best regards,
           Nomaex.
@@ -61,7 +56,7 @@ module.exports = {
         emailConfig = {
           from: process.env.NOMAEX_EMAIL,
           to: userInfo.email,
-          subject: "Order has been shipped",
+          subject: 'Order has been shipped',
           text: `Dear ${userInfo.name}, \n\n
           Thank you for shopping with NOMAEX.\n
           We have just dispatched the item(s) below from your order ${userInfo.order} in package DS-023-362654157-8338.
